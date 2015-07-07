a1cfee5404c27cb15d8ce9167c1aa260f60b0925
Jan Bölsche <regular.gonzales@googlemail.com>
2015-07-07 14:57:40 +0200

    ProcessingNode creates S- and OpCode-specific partial handlers (sub classes need to provide rhs to create full handlers)

--- processing-node.js
+++ processing-node.js
@@ -12,37 +12,78 @@ function ProcessingNode() {
 
 ProcessingNode.prototype = new ConditionalNode();
 extend(ProcessingNode.prototype.layout, {
-    Immediate:  [0x02000000,  0], // bools don't need to be shifted
     OpCode:     [0x01E00000, 21],
-    SetFlags:   [0x00100000,  0],
+    S:          [0x00100000,  0], // whether CPSR flags shoild be set by the operation
     Rn:         [0x000F0000, 16],
     Rd:         [0x0000F000, 12]
 });
 
+ProcessingNode.prototype.createUnconditionalHandler = function(ops, regs, rhs) {
+    throw new Error('ProcessingNode is an abstract Node');
+};
+
 // create code for a JS statement that performs an instruction's OpCode. Actual operation code
-// generation is delegated to methods called createXXX, where XXX is the name of the operand.
-// These createXXX methods are defined in sub-classes.
-ProcessingNode.prototype.createUnconditionalHandler = function(ops, regs) {
+// generation is delegated to methods called createXXX[S], where XXX is the name of the operator;
+// the S postfix is used for operations that set the CPRS flags (ADDS for example)
+
+// Sub classes call this to create their handlers once
+// they've figured out RHS.
+ProcessingNode.prototype._createProcessingHandler = function(ops, regs, rhs) {
     ops = ops || {};
-    var fields  = this.createMissingExtractors(ops);
-    if (typeof ops.OpCode === 'undefined') {
-        // create a generic handler
-        var code = 'switch(' + fields.OpCode + ') {\n';
+    var Rd = ops.Rd || this.createExtractor('Rd');
+    var Rn = ops.Rn || this.createExtractor('Rn');
+    var dest = regs + '[' + Rd + ']';
+    var lhs =  regs + '[' + Rn + ']';
+    var code;
+
+    function _getOperatorCode(opCode, S) {
+        var opName = operatorNames[opCode] + (S ? 'S' : '');
+        var createFunctionName = 'create' + opName;
+        if (typeof this[createFunctionName] === 'undefined') {
+            return 'throw new Error("arm32 operator not implemented:' + opName + '");';
+        } else {
+            return this[createFunctionName](ops, regs, dest, lhs, rhs);
+        }
+    }
+    getOperatorCode = _getOperatorCode.bind(this);
+   
+    function _getSwitch(S) {
+        var code = 'switch(' + this.createExtractor('OpCode') + ') {\n';
         for(var i=0; i<16; ++i) {
             code += 'case ' + i + ': ';
-            var createFunctionName = 'create' + operatorNames[i];
-            if (typeof this[createFunctionName] === 'undefined') {
-                code += 'throw new Error("arm32 operator not implemented.");';
-            } else {
-                code += this[createFunctionName](fields, regs);
-            }
+            code += getOperatorCode(i, S);
             code += 'break;\n';
         }
         code += '}\n';
         return code;
+    }
+    getSwitch = _getSwitch.bind(this);
+
+    if (typeof ops.OpCode === 'undefined') {
+        // create a generic handler
+        if (typeof ops.S === 'undefined') {
+            code = 'if (' + this.createExtractor('S') + ') {\n' +
+                getSwitch(true) +
+                '\n} else {\n' +
+                getSwitch(false) +
+                '\n}';
+            return code;
+        } else {
+            return getSwitch(ops.S);
+        }
     } else {
         // create OpCode-specific handler
-        return this['create' + operatorNames[ops.OpCode]](fields, regs);
+        var createFunctionName = 'create' + operatorNames[ops.OpCode];
+        if (typeof ops.S === 'undefined') {
+            code = 'if (' + this.createExtractor('S') + ') {' +
+                getOperatorCode(ops.OpCode, true) +
+                '} else {' +
+                getOperatorCode(ops.OpCode, false) +
+                '}';
+            return code;
+        } else {
+            return getOperatorCode(ops.OpCode, ops.S);
+        }
     }
 };
 
--- test/processing.js
+++ test/processing.js
@@ -4,43 +4,142 @@ var operatorNames = require('../arm-operator-names');
 var ProcessingNode = require('../processing-node');
 /*jshint -W054 */
 
-test('createUnconditionalHandler should create generic handler when OpCode is not specified', function(t) {
+test('should create generic handler when neither OpCode nor S is specified', function(t) {
+    t.plan(2);
     var node = new ProcessingNode();
-    node.createAND = function(fields, regs) {
-        return regs + '[' + fields.Rd + '] = "Hello from AND";';
+    node.createAND = function(ops, regs, dest) {
+        return dest + ' = "Hello from AND";';
     };
-    node.createORR = function(fields, regs) {
-        return regs + '[' + fields.Rd + '] = "Hello from ORR";';
+    node.createORRS = function(ops, regs, dest) {
+        return dest + ' = "Hello from ORRS";';
     };
-    var code = node.createUnconditionalHandler({}, 'regs');
+    var code = node._createProcessingHandler({}, 'regs');
+    console.log(code);
     var handler = new Function('instruction', 'regs', code);
     var regs = [];
     asm('AND r1, r0, r0', function(err, i) {
         handler(i, regs);
         t.equal(regs[1], 'Hello from AND');
     });
-    asm('ORR r1, r0, r0', function(err, i) {
+    asm('ORRS r1, r0, r0', function(err, i) {
         handler(i, regs);
-        t.equal(regs[1], 'Hello from ORR');
+        t.equal(regs[1], 'Hello from ORRS');
     });
-    t.end();
 });
 
-
-test('createUnconditionalHandler should create specific handler when OpCode is specified', function(t) {
+test('should create S-specific handler with flex OpCode', function(t) {
+    t.plan(4);
     var node = new ProcessingNode();
-    node.createAND = function(fields, regs) {
-        return regs + '[' + fields.Rd + '] = "Hello from AND";';
+    node.createAND = function(ops, regs, dest) {
+        return dest + ' = "Hello from AND";';
     };
-    var code = node.createUnconditionalHandler({
+    node.createORR = function(ops, regs, dest) {
+        return dest + ' = "Hello from ORR";';
+    };
+    node.createANDS = function(ops, regs, dest) {
+        return dest + ' = "Hello from ANDS";';
+    };
+    node.createORRS = function(ops, regs, dest) {
+        return dest + ' = "Hello from ORRS";';
+    };
+    var code = node._createProcessingHandler({
+        S: 0
+    }, 'regs');
+    console.log(code);
+    var handler = new Function('instruction', 'regs', code);
+    var regs = [];
+    asm('AND r10, r0, r0', function(err, i) {
+        handler(i, regs);
+        t.equal(regs[10], 'Hello from AND');
+    });
+    // NOTE: if the handler is S-specific, it will call ORR
+    // even though it is told to handle ORRS
+    asm('ORRS r11, r0, r0', function(err, i) {
+        handler(i, regs);
+        t.equal(regs[11], 'Hello from ORR');
+    });
+    code = node._createProcessingHandler({
+        S: 1
+    }, 'regs');
+    console.log(code);
+    var handlerS = new Function('instruction', 'regs', code);
+    regs = [];
+    asm('AND r10, r0, r0', function(err, i) {
+        handlerS(i, regs);
+        t.equal(regs[10], 'Hello from ANDS');
+    });
+    asm('ORRS r11, r0, r0', function(err, i) {
+        handlerS(i, regs);
+        t.equal(regs[11], 'Hello from ORRS');
+    });
+});
+
+test('should create OpCode-specific handler with flex S', function(t) {
+    t.plan(2);
+    var node = new ProcessingNode();
+    node.createAND = function(ops, regs, dest) {
+        return dest + ' = "Hello from AND";';
+    };
+    node.createANDS = function(ops, regs, dest) {
+        return dest + ' = "Hello from ANDS";';
+    };
+    var code = node._createProcessingHandler({
         OpCode: operatorNames.indexOf('AND'),
         Rd: 5
     }, 'regs');
+    console.log(code);
     var handler = new Function('instruction', 'regs', code);
     var regs = [];
-    asm('AND r1, r0, r0', function(err, i) {
+    // NOTE: AND will be called even thoufh
+    // we pass ORR – this si to prove the handler is
+    // specific to AND
+    asm('ORR r1, r0, r0', function(err, i) {
         handler(i, regs);
         t.equal(regs[5], 'Hello from AND');
     });
-    t.end();
+    asm('ORRS r1, r0, r0', function(err, i) {
+        handler(i, regs);
+        t.equal(regs[5], 'Hello from ANDS');
+    });
+});
+
+test('should create handler specific to S/OpCode combo', function(t) {
+    t.plan(2);
+    var node = new ProcessingNode();
+    node.createANDS = function(ops, regs, dest) {
+        return dest + ' = "Hello from ANDS";';
+    };
+    node.createORR = function(ops, regs, dest) {
+        return dest + ' = "Hello from ORR";';
+    };
+    var code = node._createProcessingHandler({
+        S: 1,
+        OpCode: operatorNames.indexOf('AND'),
+        Rd: 4
+    }, 'regs');
+    console.log(code);
+    var handler = new Function('instruction', 'regs', code);
+    var regs = [];
+    // NOTE: ANDS will be called even though
+    // we pass ORR – this is to prove the handler is
+    // specific to ANDS
+    asm('ORR r1, r0, r0', function(err, i) {
+        handler(i, regs);
+        t.equal(regs[4], 'Hello from ANDS');
+    });
+    code = node._createProcessingHandler({
+        S: 0,
+        OpCode: operatorNames.indexOf('ORR'),
+        Rd: 6
+    }, 'regs');
+    console.log(code);
+    var handlerORR = new Function('instruction', 'regs', code);
+    regs = [];
+    // NOTE: ORR will be called even though
+    // we pass ANDS – this is to prove the handler is
+    // specific to ORR
+    asm('ANDS r1, r0, r0', function(err, i) {
+        handlerORR(i, regs);
+        t.equal(regs[6], 'Hello from ORR');
+    });
 });
